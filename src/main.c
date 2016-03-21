b6df88b1cad7f38f4ff126a9ce34545d13899718
Geoffrey GANE <ggane@e1r4p6.42.fr>
2016-03-21 13:45:49 +0100

    git pour changement de branche

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: ggane <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/17 22:51:28 by ggane             #+#    #+#             */
-/*   Updated: 2016/03/18 00:42:59 by ggane            ###   ########.fr       */
+/*   Updated: 2016/03/21 13:14:39 by ggane            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,8 @@
 int		main(int ac, char **av)
 {
 	if (ac == 2)
-	{
-		//if (check_system_call(av[1]))
-			open_close(av[1]);
-		//else
-		//	ft_putstr("error\n");
-	}
+			if (open_close(av[1]))
+				ft_putstr("error\n");
 	else
 		ft_putstr("usage: ./fillit source_file\n");
 	return (0);
