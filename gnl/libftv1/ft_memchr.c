0b7cb483e230b238031b6b7ffe1a4ef6f1a3f7b1
Alexandre HOAREAU <ahoareau@e1r4p4.42.fr>
2016-05-06 18:17:34 +0200

    ajout gnl libft

---
+++ gnl/libftv1/ft_memchr.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahoareau <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/04/29 10:18:47 by ahoareau          #+#    #+#             */
+/*   Updated: 2016/04/29 10:18:48 by ahoareau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	unsigned char	*p;
+	unsigned int	i;
+
+	p = (unsigned char *)s;
+	i = 0;
+	while (i < n)
+	{
+		if (p[i] == (unsigned char)c)
+			return ((void *)p + i);
+		i++;
+	}
+	return (NULL);
+}
